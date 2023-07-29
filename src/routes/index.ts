e3285de0ba27116a52303609b66650da80822986
Bosipo Ojo <51823772+Bosipoo@users.noreply.github.com>
2023-07-29 11:21:09 +0100

    updates

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,19 +1,18 @@
 import { Router } from "express"; 'express';
+import authRouter from "./auth.router";
 import validateSchema from 'middlewares/validateSchema';
 import { emailSchema } from 'schemas/emails';
 import { repairDetailsSchema } from 'schemas/repairs';
 import { categorySchema, brandSchema, productSchema } from 'schemas/products';
 import { addEmailToNewsletter } from 'controllers/emails.controller';
-import authRouter from "./auth.router";
-
 import { repairAppointmentForm } from 'controllers/repairs.controller';
 import { addCategory, updateCategory, deleteCategory, getAllCategories } from 'controllers/category.controller';
 import { addBrand, updateBrand, deleteBrand, getAllBrands } from 'controllers/brands.controller';
 import { addProduct, updateProduct, deleteProduct, getAllProducts, getIdProducts, advancedSearchProducts } from 'controllers/products.controller';
 
 const baseRouter = Router();
-baseRouter.use('/auth', authRouter);
-baseRouter.post('/emails', validateSchema(emailSchema), addEmailToNewsletter)
+  baseRouter.use('/auth', authRouter);
+  baseRouter.post('/emails', validateSchema(emailSchema), addEmailToNewsletter)
   baseRouter.post('/bookRepairAppointment', validateSchema(repairDetailsSchema), repairAppointmentForm)
   baseRouter.post('/addCategory', validateSchema(categorySchema), addCategory)
   baseRouter.put('/updateCategory/:id', validateSchema(categorySchema), updateCategory)
