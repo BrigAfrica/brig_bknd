e3243da319077624769558d74a9ccc35c5d57aba
Dayo Adebanjo <dahyor@outlook.com>
2023-07-31 18:08:21 +0100

    updates

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,9 +9,11 @@ import { repairAppointmentForm } from 'controllers/repairs.controller';
 import { addCategory, updateCategory, deleteCategory, getAllCategories } from 'controllers/category.controller';
 import { addBrand, updateBrand, deleteBrand, getAllBrands } from 'controllers/brands.controller';
 import { addProduct, updateProduct, deleteProduct, getAllProducts, getIdProducts, advancedSearchProducts } from 'controllers/products.controller';
+import ordersRouter from "./orders.router";
 
 const baseRouter = Router();
   baseRouter.use('/auth', authRouter);
+  baseRouter.use('/orders', ordersRouter);
   baseRouter.post('/emails', validateSchema(emailSchema), addEmailToNewsletter)
   baseRouter.post('/bookRepairAppointment', validateSchema(repairDetailsSchema), repairAppointmentForm)
   baseRouter.post('/addCategory', validateSchema(categorySchema), addCategory)
