8a01585ce0ed0c8d56d44b803de8a43dca05033a
Dayo Adebanjo <dahyor@outlook.com>
2023-08-03 10:32:13 +0100

    fix order

--- src/routes/orders.router.ts
+++ src/routes/orders.router.ts
@@ -3,9 +3,9 @@ import { createOrder } from "controllers/order.controller";
 import { Router } from "express";
 import authMiddleware from "middlewares/authMiddleware";
 import validateSchema from "middlewares/validateSchema";
-import { orderSchema } from "schemas/orders";
+import { createOrderSchema } from "schemas/orders";
 
 const ordersRouter = Router();
-ordersRouter.post('/', authMiddleware('user'), validateSchema(orderSchema), createOrder)
+ordersRouter.post('/', authMiddleware('user'), validateSchema(createOrderSchema), createOrder)
 
 export default ordersRouter;
