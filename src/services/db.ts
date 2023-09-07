7524aecceb0346825600606c6ef71d8dfb54d0f8
Bosipo Ojo <51823772+Bosipoo@users.noreply.github.com>
2023-09-07 08:49:53 +0100

    New url

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,11 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 import postgres from 'postgres'
 
-//LOCAL URL
-//const URL = process.env.DATABASE_URL || "";
+const URL = process.env.DATABASE_URL || "";
 
-//DEPLOYED ON RENDER URL
-const URL = process.env.DATABASE_URL_RENDER_DEPLOY || "";
 
 export const sql = postgres(URL, { ssl: 'require' })
 export const prisma = new PrismaClient()
