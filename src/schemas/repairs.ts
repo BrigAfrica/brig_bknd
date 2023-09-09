a32c53f2fb2a3f7e6d7034d68bfde4ff62069b12
Bosipo Ojo <51823772+Bosipoo@users.noreply.github.com>
2023-09-09 21:20:20 +0100

    RepairType

--- src/schemas/repairs.ts
+++ src/schemas/repairs.ts
@@ -6,7 +6,7 @@ export const repairDetailsSchema = z.object({
     surname: z.string().min(2, 'Surname must be at least 2 characters').max(50, 'Surname must not exceed 50 characters'),
     email: z.string().email('Invalid email address'),
     phone: z.string().regex(/^\d{11}$/, 'Invalid phone number'), 
-    deviceType: z.enum(['Mobile Phone Repair', 'Computer Repair', 'Tablet Repair', 'Laptop Repair']),
+    deviceType: z.enum(['MobilePhone', 'Computer', 'Tablet', 'Laptop']),
     service: z.enum(['Mobile Phone Repair', 'Computer Repair', 'Tablet Repair', 'Laptop Repair']),
     dateAvailable: z.string().regex(/^\d{4}-\d{2}-\d{2}$/, 'Invalid date format. Use YYYY-MM-DD'),
     timeAvailable: z.string(),
